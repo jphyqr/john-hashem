522d16d57fd4fa7a51acb45959a92743ae8e2409
jphyqr <jphyqr@gmail.com>
2021-08-28 13:51:44 -0600

    build some records

--- next.config.js
+++ next.config.js
@@ -1,3 +1,20 @@
+require("dotenv").config();
+const webpack = require("webpack");
 module.exports = {
-  reactStrictMode: true,
-}
+  images: {
+    domains: [
+      "firebasestorage.googleapis.com",
+      "s3.us-east-2.amazonaws.com",
+      "content.homenetiol.com",
+      "items-images-production.s3.us-west-2.amazonaws.com",
+      "dl.airtable.com",
+    ],
+  },
+  webpack: (config, { isServer }) => {
+    // if (isServer) {
+    //   require("./scripts/generate-sitemap");
+    // }
+    config.plugins.push(new webpack.EnvironmentPlugin(process.env));
+    return config;
+  },
+};
